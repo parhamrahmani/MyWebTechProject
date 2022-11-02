88bcf95f461bd09e334517ccdb50407c5935a187
Parham Rahmani <parhamrahmani13762@gmail.com>
2022-11-02 22:47:09 +0100

    ProductEntity.java created.
    data columns id, brand, product_name created
    id -> primary key, not nullable
    product_name --> not nullable
    brand --> nullable = TRUE

---
+++ src/main/java/htw/berlin/webtech/MyBazaarList/web/persistence/ProductEntity.java
@@ -0,0 +1,47 @@
+package htw.berlin.webtech.MyBazaarList.web.persistence;
+
+import javax.persistence.*;
+
+@Entity(name = "products")
+public class ProductEntity {
+    protected ProductEntity() {}
+    public ProductEntity(long id, String productName, String brand) {
+        this.id = id;
+        this.productName = productName;
+        this.brand = brand;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id" ,nullable = false)
+    private long id;
+
+    @Column(name= "product_name", nullable = false)
+    private String productName;
+
+    @Column(name= "brand")
+    private String brand;
+
+
+
+    public long getId() {
+        return id;
+    }
+
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+}
