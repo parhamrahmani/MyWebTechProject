beee55ad2e57ab293e66db4a16f667df7fee36dc
Parham Rahmani <parhamrahmani13762@gmail.com>
2022-11-08 03:40:02 +0100

    enum class created -> Categories.java

---
+++ src/main/java/htw/berlin/webtech/MyBazaarList/persistence/Categories.java
@@ -0,0 +1,21 @@
+package htw.berlin.webtech.MyBazaarList.persistence;
+
+public enum Categories {
+    Fruit,
+    Vegetables,
+    Baking,
+    Beverage,
+    Bread ,
+    Breakfast ,
+    Dry_Goods,
+    Sweets,
+    Snack ,
+    Canned_Goods,
+    Condiment,
+    Dairy ,
+    Health,
+Other,
+}
+
+
+
