8a1bcd22ea0324f7f2c1d640f06686d0f7e11762
Parham Rahmani <parhamrahmani13762@gmail.com>
2022-11-12 15:31:25 +0100

    ShoppingListRepository.java interface created

---
+++ src/main/java/htw/berlin/webtech/MyBazaarList/persistence/ShoppingListRepository.java
@@ -0,0 +1,7 @@
+package htw.berlin.webtech.MyBazaarList.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShoppingListRepository extends JpaRepository<ShoppingListEntity, Long> {
+
+}
